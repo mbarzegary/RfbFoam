08659a243a44e1fe7024e9ed2514203129fd21eb
mbarzegary <mbarzegary@msn.com>
2025-07-18 17:57:13 +0200

    add ftff example case

---
+++ examples/cases/FTFF_Model/Run_U.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Start OpenFOAM
+
+#module load OpenFOAM/v2406-foss-2023a 
+#source /sw/rl8/zen/app/OpenFOAM/v2406-foss-2023a/OpenFOAM-v2406/etc/bashrc
+
+source /usr/lib/openfoam/openfoam2406/etc/bashrc
+
+decomposePar | tee log.decomposePar
+
+mpirun -np $(grep numberOfSubdomains system/decomposeParDict | awk '{print $2}' | sed 's/;//') --use-hwthread-cpus RfbFoam -onlyU -parallel | tee log.RfbFoam
+
+reconstructPar | tee log.reconstructPar
+
+rm -r processor*
+
